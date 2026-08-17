306b8b989cde32111312bf2a0716bcd706e0dc58
annapicinich1 <annapicinich1@gmail.com>
2026-08-16 23:42:47 -0500

    Update website

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Photos from './pages/Photos'
-// import About from './pages/About
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Talia-Website">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/photos" element={<Photos />} />
-        {/* <Route path="/about" element={<about/>} /> */}
       </Routes>
     </BrowserRouter>
   )
